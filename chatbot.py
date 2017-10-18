0289f771b30c852b17000ea5288913ef6804b27a
jessicac13 <32175578+jessicac13@users.noreply.github.com>
2017-10-18 15:45:15 -0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -41,3 +41,51 @@ class ChatBot(BotPlugin):
         if quantidade:
             mensagem['valor'] = quantidade
             yield(mensagem)
+            
+#batepapo
+
+
+@botcmd
+    def duvidei(self, msg, args):
+        """
+        Resposta Julinha à sua descrença.
+        """
+        yield("Duvidando? De mim? Que audácia!:rage:")
+
+    @botcmd
+    def like(self, msg, args):
+        """
+        Pede a Julinha um like.
+        """
+        yield(":thumbsup_all:")
+    
+    @re_botcmd(pattern=r"quer namorar comigo?")
+    def namoro(self, msg, args):
+        """
+        Comando do rmayormartins.
+        """
+        yield("Se eu fosse real, talvez... :heart_eyes:")
+    
+    @re_botcmd(pattern=r"posso ganhar (uma|um) (bolacha|biscoito) por favor?")
+    def biscoito_educado(self, msg, match):
+        """
+        Pede por um biscoito/bolacha para a Julinha, educadamente.
+        """
+        yield("Claro que sim, {}!").format(msg.frm)
+        yield(":cookie:")
+
+    @re_botcmd(pattern=r"quero (uma|um) (bolacha|biscoito)?")
+    def biscoito(self, msg, match):
+        """
+        Pede por um biscoito/bolacha para a Julinha.
+        """
+        yield("Seja educado {}!").format(msg.frm)
+    
+    @re_botcmd(pattern=r"[cad(e|ê)|e] o (j|J)ulinho?")
+    def julinho(self, msg, match):
+        """
+        Pergunta à Julinha sobre seu irmão.
+        """
+        yield("Está fazendo bip bip por aí...:robot_face:")
+        yield("Mas você já procurou na portaria?")
+        
